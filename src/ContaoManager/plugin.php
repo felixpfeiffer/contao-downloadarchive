86011687eb39819517c1f5bce5fe116406a91a9b
Steffen Winde <inserv@winde-ganzig.de>
2018-01-20 17:20:04 +0100

    Update plugin.php

--- src/ContaoManager/plugin.php
+++ src/ContaoManager/plugin.php
@@ -21,7 +21,7 @@ class Plugin implements BundlePluginInterface
         return [
             BundleConfig::create('FelixPfeiffer\Downloadarchive\FelixpfeifferDownloadarchive')
                 ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
-                ->setReplace(['downloadarchive']),
+                ->setReplace(['contao-downloadarchive']),
         ];
     }
 }
